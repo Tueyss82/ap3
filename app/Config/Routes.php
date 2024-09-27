471648a9e8514c6f6565e62f856f0f1287b77602
FRChanned <mathisperot420@gmail.com>
2024-09-27 15:32:43 +0200

    eh

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -5,7 +5,8 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
-$routes->get('', 'Home::index');
+$routes->get('/', 'Home::index', ['as' => 'index']);
+$routes->post('/', 'Home::index', ['as' => 'index']);
 
 $routes->get('contact', 'Home::contact');
 
