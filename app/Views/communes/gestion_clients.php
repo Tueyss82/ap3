8515a8850f77c30c66b0e9aefae6587e107edb1c
manu <emmanuel.basck@gmail.com>
2024-10-04 16:13:59 +0200

    gsxg

--- app/Views/communes/gestion_clients.php
+++ app/Views/communes/gestion_clients.php
@@ -3,19 +3,19 @@
 <section>
 
     <h1 class="table">
-        <a class="bouton" href="<?= url_to('ajout') ?>">Nouvelle commune</a>
+        <a class="bouton" href="<?= url_to('ajoutCommune') ?>">Nouvelle commune</a>
         <?php
         $table = new \CodeIgniter\View\Table();
 
         $table->setHeading(['IDCommune', 'Nom', 'Departement', 'Modifier', 'Supprimer']);
-        foreach ($gestion_clients as $commune => $va) {
+        foreach ($listeCommune as $commune => $va) {
 
             $table->addRow(
                 $va['IDCOMMUNE'],
                 $va['NOM'],
                 $va['DEPARTEMENT'],
-                '<a class="bouton" href="' . url_to('modif', $va['id']) . '">m</a>',
-                '<a class="bouton" href="' . url_to('delete', $va['id']) . '">s</a>'
+                '<a class="bouton" href="' . url_to('modifCommune', $va['IDCOMMUNE']) . '">modifier</a>',
+                '<a class="bouton" href="' . url_to('supprCommune', $va['IDCOMMUNE']) . '">supprimer</a>'
             );
 
             echo $table->generate();
