f241021e3678e1f7be1e1a1cb69976f649ecb97e
Abderrahim1516 <abderrahimkhantouch57gmail.com>
2024-11-08 15:11:01 +0100

    commit en vif

--- app/Views/panneaux/ajout_panneaux.php
+++ app/Views/panneaux/ajout_panneaux.php
@@ -2,29 +2,31 @@
 <?= $this->section('contenu') ?>
 
 <h1>Ajouter Panneaux</h1>
-<form action="<?= url_to("createPanneau") ?>" method="post">
-    <label for="name">Nom Panneau</label>
-    <input type="text" id="name" name="REFERENCE" value="">
-    <!-- <label for="name">Commune</label> -->
-    <!-- <select id="name" name="" value="">
+<div class = "centre">
+    <form action="<?= url_to("createPanneau") ?>" method="post">
+        <label for="name">Nom Panneau</label>
+        <input type="text" id="name" name="REFERENCE" value="">
+        <!-- <label for="name">Commune</label> -->
+        <!-- <select id="name" name="" value="">
         <option value="">--Choisissez une commune--</option>
         <option value="">$commune['NOM']?> - $commune['DEPARTEMENT']?></option>
     </select> -->
-    <select id="IDCOMMUNE" name = "IDCOMMUNE" value="">
-    <?php
-    foreach ($commune as $attribut) {
-            echo "<option value=" . $attribut['IDCOMMUNE'] . ">" . $attribut['NOM'] . " (" . $attribut['DEPARTEMENT'] . ")</option>";
-        }
-    ?>
-    </select>
-    <label for="latitude">Latitude</label>
-    <input type="text" id="latitude" name="LATITUDE" value="">
+        <select id="IDCOMMUNE" name="IDCOMMUNE" value="">
+            <?php
+            foreach ($commune as $attribut) {
+                echo "<option value=" . $attribut['IDCOMMUNE'] . ">" . $attribut['NOM'] . " (" . $attribut['DEPARTEMENT'] . ")</option>";
+            }
+            ?>
+        </select>
+        <label for="latitude">Latitude</label>
+        <input type="text" id="latitude" name="LATITUDE" value="">
 
-    <label for="longitude">Longitude</label>
-    <input type="text" id="longitude" name="LONGITUDE" value="">
+        <label for="longitude">Longitude</label>
+        <input type="text" id="longitude" name="LONGITUDE" value="">
 
-    <input type="submit" value="Valider">
-</form>
+        <input type="submit" value="Valider">
+    </form>
+</div>
 
 
 <?= $this->endSection() ?>
