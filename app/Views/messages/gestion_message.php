efe2c800ce521cebe1003a4c1d2ee7b30dc22978
mvhvmmvvvvd <156082995+mvhvmmvvvvd@users.noreply.github.com>
2024-11-11 14:59:00 +0100

    Update gestion_message.php

--- app/Views/messages/gestion_message.php
+++ app/Views/messages/gestion_message.php
@@ -1,23 +1,24 @@
 <?= $this->extend('layout') ?>
 <?= $this->section('contenu') ?>
-<!-- CONTENT -->
 
 <section>
-    <a class="bouton" href="<?= url_to('ajout_message') ?>">Ajouter un message</a>
-    <?php
+    <a class="bouton" href="<?= url_to('ajoutMessage') ?>">Ajouter un message</a>
 
+    <?php
     $table = new \CodeIgniter\View\Table();
-    $table->setHeading('ETAT', 'TEXTE', 'COULEUR', 'TAILLE', 'Modifier', 'Supprimer');
+    $table->setHeading('État', 'Texte', 'Couleur', 'Taille', 'Modifier', 'Supprimer');
+
     foreach ($messages as $message) {
         $table->addRow(
             $message['ETAT'],
             $message['TEXTE'],
             $message['COULEUR'],
             $message['TAILLE'],
-            '<a class="bouton" href="' . url_to('modifier_message', $message['IDMESSAGE']) . '">Modifier</a>',
-            '<a class="bouton" href="' . url_to('suppr_message', $message['IDMESSAGE']) . '">Supprimer</a>'
+            '<a class="bouton" href="' . url_to('modifMessage', $message['IDMESSAGE']) . '">Modifier</a>',
+            '<a class="bouton" href="' . url_to('supprMessage', $message['IDMESSAGE']) . '">Supprimer</a>'
         );
     }
+
     echo $table->generate();
     ?>
 </section>
